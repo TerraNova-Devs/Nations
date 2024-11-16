ec54ffaf0a9b20a4bed50ede01553a856ca4ae3a
VonNekyia <germarten@gmail.com>
2024-11-16 14:44:58 +0100

    working towards new rank mechanic (just internal)

--- src/main/java/de/terranova/nations/commands/terraSubCommands/TerraAccessSubCommand.java
+++ src/main/java/de/terranova/nations/commands/terraSubCommands/TerraAccessSubCommand.java
@@ -5,6 +5,7 @@ import de.terranova.nations.commands.SubCommand;
 import de.terranova.nations.commands.TerraSelectCache;
 import de.terranova.nations.database.SettleDBstuff;
 import de.terranova.nations.settlements.AccessLevel;
+import de.terranova.nations.worldguard.RegionClaimFunctions;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import org.bukkit.Bukkit;
@@ -14,7 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+//ask join and leave commands, update pl3xmap after changes
 public class TerraAccessSubCommand extends SubCommand implements BasicCommand {
     public TerraAccessSubCommand(String permission) {
         super(permission);
@@ -33,16 +34,16 @@ public class TerraAccessSubCommand extends SubCommand implements BasicCommand {
             p.sendMessage(Chat.errorFade("Bitte nutze /t user <add|remove|rank|resign> <username> (<rank>)"));
             return;
         }
-        AccessLevel targetAccessLevel = null;
+        AccessLevel inputAccessLevel = null;
         if(args.length == 4) {
             for(AccessLevel level : AccessLevel.values()) {
                 if(level.name().equalsIgnoreCase(args[3])) {
-                    targetAccessLevel = level;
+                    inputAccessLevel = level;
                     break;
                 }
             }
-            if(targetAccessLevel == null) {
-                p.sendMessage(Chat.errorFade(String.format("Der Spieler %s konnte nicht gefunden werden", args[2])));
+            if(inputAccessLevel == null) {
+                p.sendMessage(Chat.errorFade(String.format("Der Spieler %s konnte nicht gefunden werden", args[3])));
                 return;
             }
         }
@@ -51,9 +52,9 @@ public class TerraAccessSubCommand extends SubCommand implements BasicCommand {
             p.sendMessage(Chat.errorFade(String.format("Der Spieler %s konnte nicht gefunden werden", args[2])));
             return;
         }
-        AccessLevel targetAccess = null;
+        AccessLevel targetAccessLevel = null;
         if(cache.getSettle().accessLevel.containsKey(target.getUniqueId())){
-            targetAccess = cache.getSettle().accessLevel.get(target.getUniqueId());
+            targetAccessLevel = cache.getSettle().accessLevel.get(target.getUniqueId());
         };
 
         if(!hasAccess(cache.getAccess(), AccessLevel.VICE)) {
@@ -62,40 +63,65 @@ public class TerraAccessSubCommand extends SubCommand implements BasicCommand {
         }
 
         if(args[1].equalsIgnoreCase("add")) {
-            if(targetAccess != null) {
+            if(targetAccessLevel != null) {
                 p.sendMessage(String.format("Der Spieler %s ist bereits Mitglied deiner Stadt.",target.getName()));
                 return;
             }
+
+            settleDB.changeMemberAccess(target.getUniqueId(), AccessLevel.CITIZEN);
+            RegionClaimFunctions.addOrRemoveFromSettlement(target, cache.getSettle(), true);
+            cache.getSettle().accessLevel.put(target.getUniqueId(), AccessLevel.CITIZEN);
+
+            target.sendMessage(Chat.greenFade(String.format("Du wurdest zu der Region %s als Mitglied hinzugefügt.",cache.getSettle().name)));
+            p.sendMessage(Chat.greenFade(String.format("Du hast %s erfolgreich zur Stadt %s als Mitglied hinzugefügt.",target.getName(),cache.getSettle().name)));
         }
         else if(args[1].equalsIgnoreCase("remove")) {
-            if(targetAccess == null) {
-                p.sendMessage(String.format("Der Spieler %s kann nicht entfernt werden, er ist kein Mitglied deiner Stadt.",target.getName()));
+            if(targetAccessLevel == null) {
+                p.sendMessage(Chat.errorFade(String.format("Der Spieler %s kann nicht entfernt werden, er ist kein Mitglied deiner Stadt.",target.getName())));
                 return;
             }
+
+            settleDB.changeMemberAccess(target.getUniqueId(), null);
+            RegionClaimFunctions.addOrRemoveFromSettlement(target, cache.getSettle(), false);
+            cache.getSettle().accessLevel.remove(target.getUniqueId());
+
+            target.sendMessage(Chat.redFade(String.format("Du wurdest von der Region %s als Mitglied entfernt.",cache.getSettle().name)));
+            p.sendMessage(Chat.redFade(String.format("Du hast %s erfolgreich von der Stadt %s entfernt.",target.getName(),cache.getSettle().name)));
         }
         else if(args[1].equalsIgnoreCase("rank")) {
             if(!(args.length == 4)) {
                 p.sendMessage(Chat.errorFade("Bitte nutze /t user rank <username> <rank>"));
                 return;
             }
-            if(targetAccess == null) {
+            if(targetAccessLevel == null) {
                 p.sendMessage(String.format("Der Spieler %s ist kein Mitglied deiner Stadt und kann demnach nicht befördert werden.",target.getName()));
                 return;
             }
-            settleDB.changeMemberAccess(target.getUniqueId(), AccessLevel.MAJOR);
 
+            settleDB.changeMemberAccess(target.getUniqueId(), inputAccessLevel);
+            cache.getSettle().accessLevel.replace(target.getUniqueId(), inputAccessLevel);
+
+            target.sendMessage(Chat.redFade(String.format("Dein Rang wurde in der Region %s auf %s geändert.",cache.getSettle().name,inputAccessLevel.name())));
+            p.sendMessage(Chat.redFade(String.format("Du hast %s erfolgreich auf den Rang %s gestuft.",target.getName(),inputAccessLevel.name())));
         }
         else if(args[1].equalsIgnoreCase("resign")) {
             if(!hasAccess(cache.getAccess(), AccessLevel.MAJOR)) {
                 p.sendMessage("Du müsst Bürgermeister einer Stadt sein um diese an einen anderen Spieler überschreiben zu können");
                 return;
             }
-            if(targetAccess == null || !hasAccess(targetAccess, AccessLevel.VICE)) {
+            if(targetAccessLevel == null || !hasAccess(targetAccessLevel, AccessLevel.VICE)) {
                 p.sendMessage("Du kannst deine Stadt aus Sicherheitsgründen nur an einen Vizeanführer übergeben.");
+                return;
+            }
+            boolean hasConsent = false;
+            if(!hasConsent) {
+                p.sendMessage("Dein gegenüber hat dem Transfer der Stadt nicht zugestimmt.");
                 return;
             }
             settleDB.changeMemberAccess(p.getUniqueId(), null);
             settleDB.changeMemberAccess(target.getUniqueId(), AccessLevel.MAJOR);
+            target.sendMessage(Chat.redFade(String.format("Dir wurde erfolgreich die Stadt %s übertragen.",cache.getSettle().name)));
+            p.sendMessage(Chat.redFade(String.format("Du hast %s erfolgreich die Stadt %s übertragen.",target.getName(),cache.getSettle().name)));
         } else {
             p.sendMessage(Chat.errorFade("Bitte nutze /t user <add|remove|rank|resign> <username> (<rank>)"));
             return;
