5ad668d8bac17401d4ae39a0dea21a789d779267
VonNekyia <germarten@gmail.com>
2024-12-11 00:56:42 +0100

    bug fixes -> npc duplication, map updates on region creation

--- src/main/java/de/terranova/nations/pl3xmap/RegionLayer.java
+++ src/main/java/de/terranova/nations/pl3xmap/RegionLayer.java
@@ -36,7 +36,7 @@ public class RegionLayer extends WorldLayer {
             throw new RuntimeException(e);
         }
 
-        setUpdateInterval(10);
+        setUpdateInterval(200);
         setLiveUpdate(true);
         setShowControls(true);
         setDefaultHidden(false);
@@ -47,8 +47,9 @@ public class RegionLayer extends WorldLayer {
     static Map<String,Marker<?>> markers = new HashMap<>();
 
     public static void updateRegion(SettleRegionType region){
+        System.out.println("DEBUG Updating region 0" + region);
         if(markers.containsKey(region.getId() + "icon")) removeRegion(region.getId());
-
+        System.out.println("DEBUG Updating region 1" + region);
         if(region.getWorldguardRegion() == null) {
             Bukkit.getLogger().severe(String.format("Wordguard Region wurde für %s nicht gefunden!",region.getName()));
             return;
@@ -82,14 +83,16 @@ public class RegionLayer extends WorldLayer {
                 .fillColor(0x5540E53F)
                 .strokeColor(0xDD8640E6)
                 .build();
-
+        System.out.println("DEBUG Updating region 2" + region);
+        System.out.println("DEBUG" + region.getId() + "" +  region.getLocation().x + "" + region.getLocation().z);
+        System.out.println("DEBUG" + optionspoly);
         markers.put(region.getId().toString() + "icon",Marker.icon("icon" + region.getId(), region.getLocation().x, region.getLocation().z, "castle", 32).setOptions(optionsicon));
         markers.put(region.getId().toString() + "area",polygonMarker.setOptions(optionspoly));
     }
 
     public static void removeRegion(UUID regionID){
-        markers.remove(regionID+ "icon");
-        markers.remove(regionID+ "area");
+        markers.remove(regionID.toString() + "icon");
+        markers.remove(regionID + "area");
     }
 
     @Override
