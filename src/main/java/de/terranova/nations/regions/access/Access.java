9658e23d7971c327d3ac14d4fc4952bd9da7cc61
VonNekyia <germarten@gmail.com>
2025-01-01 13:37:49 +0100

    fixed bugs in access and added new invite function + removing of offline players

--- src/main/java/de/terranova/nations/regions/access/Access.java
+++ src/main/java/de/terranova/nations/regions/access/Access.java
@@ -1,5 +1,6 @@
 package de.terranova.nations.regions.access;
 
+import de.mcterranova.terranovaLib.utils.Chat;
 import de.terranova.nations.regions.RegionManager;
 import de.terranova.nations.regions.base.RegionType;
 import de.terranova.nations.regions.base.RegionTypeListener;
@@ -22,40 +23,15 @@ public class Access implements RegionTypeListener {
         this.accessDatabase = new AccessDatabase(regionType.getId());
         this.accessLevel = accessDatabase.getMembersAccess();
         regionType.addListener(this);
-
-    }
-
-    public HashMap<UUID, AccessLevel> getAccessLevels() {
-        return this.accessLevel;
     }
 
     public void setAccessLevels(HashMap<UUID, AccessLevel> accessLevels) {
         this.accessLevel = accessLevels;
     }
 
-    public UUID getMajor() {
-        return accessLevel.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue() == AccessLevel.MAJOR)
-                .map(Map.Entry::getKey)
-                .findFirst()
-                .orElse(null);
+    public HashMap<UUID, AccessLevel> getAccessLevels() {
+        return this.accessLevel;
     }
-
-    public List<UUID> getAllUUIDsOfLevel(AccessLevel level) {
-        return accessLevel.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue() == level)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-    }
-
-    public void removeAccess(UUID uuid) {
-        HashMap<UUID, AccessLevel> accessLevels = getAccessLevels();
-        accessLevels.remove(uuid);
-        accessDatabase.changeMemberAccess(uuid, null);
-        setAccessLevels(accessLevels);
-    };
 
     public void setAccessLevel(UUID uuid, AccessLevel accessLevel){
         HashMap<UUID, AccessLevel> accessLevels = getAccessLevels();
@@ -68,38 +44,56 @@ public class Access implements RegionTypeListener {
         setAccessLevels(accessLevels);
     }
 
+    public AccessLevel getAccessLevel(UUID uuid) {
+        if(getAccessLevels().containsKey(uuid)) return getAccessLevels().get(uuid);
+        return null;
+    }
+
+    public void removeAccess(UUID uuid) {
+        HashMap<UUID, AccessLevel> accessLevels = getAccessLevels();
+        accessLevels.remove(uuid);
+        accessDatabase.changeMemberAccess(uuid, null);
+        setAccessLevels(accessLevels);
+    };
+
     public static boolean hasAccess(AccessLevel access, AccessLevel neededAcess) {
         if (access == null) return false;
         return access.getWeight() >= neededAcess.getWeight();
     }
 
-    public AccessLevel getAccessLevel(UUID uuid) {
-        if(getAccessLevels().containsKey(uuid)) {
-            return getAccessLevels().get(uuid);
-        }
-        return null;
-    }
-
     public Collection<UUID> getEveryUUIDWithCertainAccessLevel(AccessLevel access) {
-        Collection<UUID> output = new ArrayList<>();
-        for (UUID uuid : accessLevel.keySet()) {
-            if (accessLevel.get(uuid).equals(access)) {
-                output.add(uuid);
-            }
-        }
-        return output;
+        return accessLevel.keySet()
+                .stream().filter(uuid -> accessLevel.get(uuid).equals(access))
+                .collect(Collectors.toSet());
     }
 
     public Collection<String> getEveryMemberNameWithCertainAccessLevel(AccessLevel access) {
-        Collection<String> output = new ArrayList<>();
-        for (UUID uuid : accessLevel.keySet()) {
-            if (accessLevel.get(uuid).equals(access)) {
-                OfflinePlayer p = Bukkit.getOfflinePlayer(uuid);
-                if (p.getName() == null) continue;
-                output.add(p.getName());
-            }
-        }
-        return output;
+        return accessLevel.keySet()
+                .stream().filter(uuid -> accessLevel.get(uuid).equals(access))
+                .map(uuid -> Bukkit.getOfflinePlayer(uuid).getName())
+                .filter(Objects::nonNull).collect(Collectors.toSet());
+    }
+
+    public UUID getMajor() {
+        return accessLevel.entrySet()
+                .stream().filter(entry -> entry.getValue() == AccessLevel.MAJOR)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<UUID> getAllUUIDsOfLevel(AccessLevel level) {
+        return accessLevel.entrySet()
+                .stream().filter(entry -> entry.getValue() == level)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public void broadcast(String message) {
+        accessLevel.keySet()
+                .stream().map(Bukkit::getPlayer)
+                .filter(Objects::nonNull)
+                .forEach(p -> p.sendMessage(Chat.greenFade(message)));
     }
 
     @Override
