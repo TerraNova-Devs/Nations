3968530a364c612358befd66893bcbc200f2ebea
Bastizeit <s.tuetti@gmx.de>
2025-02-27 20:58:24 +0100

    Change map colors depending on Nationname

--- src/main/java/de/terranova/nations/nations/NationManager.java
+++ src/main/java/de/terranova/nations/nations/NationManager.java
@@ -1,8 +1,13 @@
 package de.terranova.nations.nations;
 
 import de.terranova.nations.database.dao.NationsDAO;
+import de.terranova.nations.pl3xmap.RegionLayer;
+import de.terranova.nations.regions.RegionManager;
+import de.terranova.nations.regions.grid.SettleRegion;
 
 import java.util.*;
+
+import static de.terranova.nations.database.dao.NationsDAO.saveNation;
 
 public class NationManager {
     private Map<UUID, Nation> nations;
@@ -21,12 +26,24 @@ public class NationManager {
     public void addNation(Nation nation) {
         nations.put(nation.getId(), nation);
         saveNation(nation);
+        for (UUID settlementId : nation.getSettlements().keySet()) {
+            NationsDAO.addSettlementToNation(new SettlementNationRelation(settlementId, nation.getId(), nation.getSettlements().get(settlementId)));
+            Optional<SettleRegion> settleRegion = RegionManager.retrieveRegion("settle", settlementId);
+            RegionLayer.updateRegion(settleRegion.get());
+        }
+
     }
 
     // Remove a nation from the manager and database
     public void removeNation(UUID nationId) {
-        nations.remove(nationId);
-        NationsDAO.deleteNation(nationId);
+       Nation nation = nations.get(nationId);
+       for (UUID settlementId : nation.getSettlements().keySet()) {
+           Optional<SettleRegion> settleRegion = RegionManager.retrieveRegion("settle", settlementId);
+           RegionLayer.updateRegion(settleRegion.get());
+       }
+
+       nations.remove(nationId);
+       NationsDAO.deleteNation(nationId);
     }
 
     // Get a nation by UUID
@@ -100,19 +117,30 @@ public class NationManager {
 
     // Save a nation to the database
     public void saveNation(Nation nation) {
-        NationsDAO.saveNation(nation);
         nations.put(nation.getId(), nation);
+        for (UUID settlementId : nation.getSettlements().keySet()) {
+            Optional<SettleRegion> settleRegion = RegionManager.retrieveRegion("settle", settlementId);
+            RegionLayer.updateRegion(settleRegion.get());
+        }
     }
 
     public void addSettlementToNation(UUID nationId, UUID settlementId) {
         NationsDAO.addSettlementToNation(new SettlementNationRelation(settlementId, nationId, SettlementRank.CITY));
         Nation nation = getNation(nationId);
         nation.addSettlement(settlementId, SettlementRank.CITY);
+
+        // Update region layer
+        Optional<SettleRegion> settleRegion = RegionManager.retrieveRegion("settle", settlementId);
+        RegionLayer.updateRegion(settleRegion.get());
     }
 
     public void removeSettlementFromNation(UUID nationId, UUID settlementId) {
         NationsDAO.removeSettlementFromNation(settlementId);
         Nation nation = getNation(nationId);
         nation.removeSettlement(settlementId);
+
+        // Update region layer
+        Optional<SettleRegion> settleRegion = RegionManager.retrieveRegion("settle", settlementId);
+        RegionLayer.updateRegion(settleRegion.get());
     }
 }
