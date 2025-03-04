16820a8a40a338675c7e5cf66613f982d53977ea
Bastizeit <s.tuetti@gmx.de>
2025-03-04 15:42:26 +0100

    Change town tooltip to show new nation banner & name + restructuring

--- src/main/java/de/terranova/nations/pl3xmap/RegionLayer.java
+++ src/main/java/de/terranova/nations/pl3xmap/RegionLayer.java
@@ -2,6 +2,7 @@ package de.terranova.nations.pl3xmap;
 
 import de.terranova.nations.regions.access.TownAccessLevel;
 import de.terranova.nations.regions.grid.SettleRegion;
+import de.terranova.nations.utils.BannerRenderer;
 import de.terranova.nations.utils.ColorUtils;
 import de.terranova.nations.worldguard.math.Vectore2;
 import net.pl3x.map.core.Pl3xMap;
@@ -17,6 +18,8 @@ import net.pl3x.map.core.util.FileUtil;
 import net.pl3x.map.core.world.World;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BannerMeta;
 import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
@@ -72,38 +75,100 @@ public class RegionLayer extends WorldLayer {
             owner = Bukkit.getOfflinePlayer(ownerId).getName();
         }
 
-        String tooltip = String.format(
-                "<style> @font-face { font-family: minecraft; src: url('images/font/Minecrafter.Reg.ttf'); } p { font-family: minecraft; text-align: center; margin-top: 0; margin-bottom: 0; color:#D9D9D9; } p.mid { text-align: left; } p.color{ color: #68D9B0; font-size: 30px;} </style>"
-                        + "<center><p class='color'>%s</p></center>"
-                        + "<br><center><img src='images/banner/Mitglieder.png' height='50' width='200' ></center>"
-                        + "<br><p class='mid'>Owner: %s<br>Vize%s<br>Council%s</p>"
-                        + "<br><center><img src='images/banner/Statistiken.png' height='50' width='200' ></center>"
-                        + "<br><p class='mi'>Level: %s<br>Claims: %s/%s</p>",
-                region.getName().replace("_", " "),
-                owner,
-                ": " + StringUtils.join(vices, ", "),
-                ": " + StringUtils.join(councils, ", "),
-                region.getRank().getLevel(),
-                region.getClaims(),
-                region.getMaxClaims()
-        );
+        String regionName = region.getName().replace("_", " ");
 
         // 3) Determine fill/stroke color based on the settlement's nation
-        //    (Replace 'YourNationManagerClass' with your actual manager)
-        int fillColor = 0x5540E53F;  // default if no nation
-        int strokeColor = 0xDD8640E6; // default stroke
+        int fillColor = 0x5540E53F;  // default
+        int strokeColor = 0xDD8640E6;
+        String nationColorHex = "#68D9B0"; // fallback if no nation
 
-        // Suppose you have something like:
-        // Nation nation = NationsPlugin.nationManager.getNationBySettlement(region.getId());
-        // If it exists, use a color derived from the nation's name
         var nation = de.terranova.nations.NationsPlugin.nationManager.getNationBySettlement(region.getId());
+        String nationName = "No Nation";
+        String bannerHtml = "";
+
         if (nation != null) {
-            fillColor = ColorUtils.getColorFromName(nation.getName());    // from step 1
-            // For stroke, you might lighten/darken it:
-            strokeColor = (fillColor & 0x00FFFFFF) | 0xDD000000; // same hue, a bit more opaque, etc.
+            fillColor = ColorUtils.getColorFromName(nation.getName());
+            strokeColor = (fillColor & 0x00FFFFFF) | 0xDD000000;
+            nationName = nation.getName();
+
+            // convert fillColor -> #RRGGBB (ignoring alpha)
+            nationColorHex = String.format("#%06X", (0xFFFFFF & fillColor));
+
+            // Render banner -> data URI
+            ItemStack bannerItem = nation.getBanner();
+            if (bannerItem != null && bannerItem.getType().name().contains("BANNER")) {
+                String dataUri = BannerRenderer.renderBannerToDataURI(bannerItem);
+                if (dataUri != null) {
+                    // Put banner in a small <img> on the right
+                    bannerHtml = "<img src=\"" + dataUri + "\" style=\"opacity:1.0;\" width=\"44\" height=\"80\" />";
+                }
+            }
+
+            if(nation.getCapital().equals(region.getId())){
+                regionName = "✪ " + regionName;
+            }
         }
 
-        // 4) Build Options for icon’s tooltip
+        String tooltip = String.format("""
+                <style>
+                  @font-face {
+                    font-family: minecraft; 
+                    src: url('images/font/Minecrafter.Reg.ttf'); 
+                  }
+                  p, h2 {
+                    font-family: minecraft;
+                    margin: 2px;
+                    color: #D9D9D9;
+                  }
+                  .title {
+                    font-size: 26px; 
+                    color: %s; /* use the nation color if available */
+                  }
+                  .row {
+                    display: flex; 
+                    justify-content: space-between; 
+                    align-items: flex-start;
+                  }
+                  .row-bottom {
+                    display: flex; 
+                    justify-content: space-around; 
+                    margin-top: 10px;
+                  }
+                </style>
+                
+                <!-- Top row: Text on left, Banner on right -->
+                <div class="row">
+                  <div>
+                    <!-- Town Name / Nation Info -->
+                    <h2 class="title">%s</h2>
+                    <p>Nation: %s<br>Level: %s<br>Claims: %s/%s</p>
+                  </div>
+                  <div>
+                    %s
+                  </div>
+                </div>
+                
+                <!-- Bottom row: two side-by-side boxes -->
+                <div class="row-bottom">
+                  <div>
+                    <img src="images/banner/Mitglieder.png" height="50" width="200" />
+                    <p>Owner: %s<br>Vize: %s<br>Council: %s</p>
+                  </div>
+                </div>
+                """,
+                nationColorHex,
+                regionName,
+                nationName,
+                region.getRank().getLevel(),
+                region.getClaims(),
+                region.getMaxClaims(),
+                bannerHtml,
+                owner,
+                ": " + StringUtils.join(vices, ", "),
+                ": " + StringUtils.join(councils, ", ")
+
+        );
+
         Options optionsicon = Options.builder()
                 .tooltipContent(tooltip)
                 .tooltipDirection(Tooltip.Direction.TOP)
