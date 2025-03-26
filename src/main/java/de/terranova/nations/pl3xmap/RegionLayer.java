bf3db82143550ffe9f96b721cc74a547a83ea500
Bastizeit <s.tuetti@gmx.de>
2025-03-26 19:39:26 +0100

    Add Professions to RegionLayer of Pl3xMap

--- src/main/java/de/terranova/nations/pl3xmap/RegionLayer.java
+++ src/main/java/de/terranova/nations/pl3xmap/RegionLayer.java
@@ -177,7 +177,6 @@ public class RegionLayer extends WorldLayer {
                   </div>
                 </div>
                 
-                <!-- Middle row with the “Mitglieder” image, owner, vize, council -->
                 <div class="row-bottom">
                   <div>
                     <img src="images/banner/Mitglieder.png" height="50" width="200" />
@@ -185,7 +184,6 @@ public class RegionLayer extends WorldLayer {
                   </div>
                 </div>
                 
-                <!-- NEW: Put the profession dots here, in a separate block so they appear BELOW everything else -->
                 <div class="professions-block">
                   <img src="images/banner/Statistiken.png" height="50" width="200" />
                   %s
