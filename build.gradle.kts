f4fcd2191f6e371e1d9689a347170607f3b4fde9
Bastizeit <s.tuetti@gmx.de>
2025-04-01 11:19:42 +0200

    Fix TownSkinGUI by fixing setSkull in TerranovaLib

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
   compileOnly(fileTree(mapOf("dir" to "jars", "include" to listOf("*.jar"))))
   implementation("io.github.cdimascio:dotenv-java:3.0.0")
   compileOnly("com.nexomc:nexo:1.1.0")
-  implementation("de.mcterranova:terranova-lib:1.0.0")
+  implementation("de.mcterranova:terranova-lib:1.0.1")
   implementation("org.yaml:snakeyaml:2.2")
 }
 
